b7d078c18477e29faa3b09773dd85dac7e9102e8
jun39 <bbbbb12312339@gmail.com>
2020-04-20 17:16:42 +0900

    comparisonAppを削除

--- comparisonApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComparisonappConfig(AppConfig):
-    name = 'comparisonApp'
--- jobanalysis/settings.py
+++ jobanalysis/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inputApp.apps.InputappConfig',
-    'comparisonApp',
     'accounts.apps.AccountsConfig',
 ]
 
